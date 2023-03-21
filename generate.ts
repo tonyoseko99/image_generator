5c223280b063b18aea0194df16478f02a7c548ca
Tonny Oseko <tonnyoseko0@gmail.com>
2023-03-21 16:33:26 +0300

    add get and post routes for the forms

--- generate.ts
+++ generate.ts
@@ -11,18 +11,56 @@ declare var process: {
 };
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 const port = 3000;
 const leap = new Leap(process.env.LEAP_API_KEY);
 
 // create a route
 app.get("/", async (req, res) => {
   res.send(`
-    <form method="post" action="/generate">
-      <label for="prompt">Enter your prompt:</label><br>
-      <input type="text" id="prompt" name="prompt"><br>
-      <input type="submit" value="Submit">
-    </form>
-  `);
+      <html>
+        <head>
+          <style>
+            /* Add CSS rules here */
+            form {
+              margin: 20px;
+              padding: 10px;
+              border: 1px solid black;
+            }
+            label {
+              font-size: 18px;
+              margin-bottom: 5px;
+            }
+            input[type="text"] {
+              width: 100%;
+              padding: 10px;
+              font-size: 16px;
+              border-radius: 5px;
+              border: 1px solid #ccc;
+              margin-bottom: 10px;
+            }
+            input[type="submit"] {
+              background-color: #4CAF50;
+              color: white;
+              padding: 12px 20px;
+              border: none;
+              border-radius: 4px;
+              cursor: pointer;
+            }
+            input[type="submit"]:hover {
+              background-color: #45a049;
+            }
+          </style>
+        </head>
+        <body>
+          <form method="post" action="/generate">
+            <label for="prompt">Enter your prompt:</label><br>
+            <input type="text" id="prompt" name="prompt"><br>
+            <input type="submit" value="Submit">
+          </form>
+        </body>
+      </html>
+    `);
 });
 
 app.post("/generate", async (req, res) => {
@@ -36,8 +74,20 @@ app.post("/generate", async (req, res) => {
   }
 
   res.send(`
-        <img src="${data?.images[0].uri}" alt="Generated image" />
-    `);
+  <style>
+    /* Add CSS rules here */
+    img {
+      display: block;
+      margin: 0 auto;
+      border: 1px solid black;
+      max-width: 100%;
+    }
+  </style>
+  <img src="${data?.images[0].uri}" alt="Generated image" />
+`);
 });
 
-
+// start the Express server
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
