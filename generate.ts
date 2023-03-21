e07147e27c0922e799cbf83230055e286e6a40d6
Tonny Oseko <tonnyoseko0@gmail.com>
2023-03-21 16:06:29 +0300

    add get and post routes for the forms

--- generate.ts
+++ generate.ts
@@ -14,3 +14,30 @@ const app = express();
 const port = 3000;
 const leap = new Leap(process.env.LEAP_API_KEY);
 
+// create a route
+app.get("/", async (req, res) => {
+  res.send(`
+    <form method="post" action="/generate">
+      <label for="prompt">Enter your prompt:</label><br>
+      <input type="text" id="prompt" name="prompt"><br>
+      <input type="submit" value="Submit">
+    </form>
+  `);
+});
+
+app.post("/generate", async (req, res) => {
+  const prompt = req.body.prompt;
+  const { data, error } = await leap.generate.generateImage({ prompt });
+
+  if (error) {
+    res.status(500).send({
+      error: error.message,
+    });
+  }
+
+  res.send(`
+        <img src="${data?.images[0].uri}" alt="Generated image" />
+    `);
+});
+
+
